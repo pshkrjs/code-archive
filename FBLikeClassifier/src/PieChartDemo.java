8cc70aaf6cc34f88d2d9405c783fd5916f320a61
Pushkaraj Shinde <pushkaraj.shinde@hotmail.com>
2023-11-21 11:14:16 +0530

    Adding FB Like Classifier in Java

---
+++ FBLikeClassifier/src/PieChartDemo.java
@@ -0,0 +1,91 @@
+
+
+//package org.jfree.chart.demo;
+
+import java.awt.Font;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+/**
+ * A simple demonstration application showing how to create a pie chart using
+ * data from a {@link DefaultPieDataset}.
+ */
+public class PieChartDemo extends ApplicationFrame {
+
+	/**
+	 * Default constructor.
+	 *
+	 * @param title
+	 *            the frame title.
+	 */
+	public PieChartDemo(String title) {
+		super(title);
+		setContentPane(createDemoPanel());
+	}
+
+	/**
+	 * Creates a sample dataset.
+	 * 
+	 * @return A sample dataset.
+	 */
+	private static PieDataset createDataset() {
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		dataset.setValue("Technical", new Double(var.count1));
+		dataset.setValue("Entertainment", new Double(var.count2));
+		dataset.setValue("Other", new Double(var.count3));
+
+		return dataset;
+	}
+
+	/**
+	 * Creates a chart.
+	 * 
+	 * @param dataset
+	 *            the dataset.
+	 * 
+	 * @return A chart.
+	 */
+	private static JFreeChart createChart(PieDataset dataset) {
+
+		JFreeChart chart = ChartFactory.createPieChart("Facebook user likes classification", // chart
+																								// title
+				dataset, // data
+				true, // include legend
+				true, false);
+
+		PiePlot plot = (PiePlot) chart.getPlot();
+		plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 12));
+		plot.setNoDataMessage("No data available");
+		plot.setCircular(false);
+		plot.setLabelGap(0.02);
+		return chart;
+
+	}
+
+	/**
+	 * Creates a panel for the demo (used by SuperDemo.java).
+	 * 
+	 * @return A panel.
+	 */
+	public static JPanel createDemoPanel() {
+		JFreeChart chart = createChart(createDataset());
+		return new ChartPanel(chart);
+	}
+
+	/**
+	 * Starting point for the demonstration application.
+	 *
+	 * @param args
+	 *            ignored.
+	 */
+
+}
---
+++ FBLikeClassifier/src/var.java
@@ -0,0 +1,6 @@
+public class var {
+	public static int count1 = 0;
+	public static int count2 = 0;
+	public static int count3 = 0;
+
+}
