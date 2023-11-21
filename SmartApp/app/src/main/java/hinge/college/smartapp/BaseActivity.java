acffe051b344ff1cdc982d73264b83cb1e4911d1
Pushkaraj Shinde <pushkaraj.shinde@hotmail.com>
2023-11-21 11:17:12 +0530

    Adding Smart App in Java

---
+++ SmartApp/app/src/main/java/hinge/college/smartapp/BaseActivity.java
@@ -0,0 +1,36 @@
+package hinge.college.smartapp;
+
+/**
+ * Created by push on 9/9/16.
+ */import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
---
+++ SmartApp/app/src/main/java/hinge/college/smartapp/MainActivity.java
@@ -0,0 +1,52 @@
+package hinge.college.smartapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://smartapp-64e76.appspot.com");
+
+        String path = "/storage/emulated/0/Download/river.jpg";
+        Uri file = Uri.fromFile(new File(path));
+        StorageReference riversRef = storageRef.child("images/"+file.getLastPathSegment());
+        UploadTask uploadTask = riversRef.putFile(file);
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Log.d("file upload", "onSuccess: "+downloadUrl.toString());
+            }
+        });
+
+        //Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+        //startActivity(intent);
+    }
+}
